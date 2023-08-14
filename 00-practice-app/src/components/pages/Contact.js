073c027d0de2c869c9357f48fbeac354b7e7d801
JZERO MAKILING <jzerorobby07092016@gmail.com>
2023-08-13 21:45:24 -0700

    Added files and folders

---
+++ 00-practice-app/src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <ul>
+        <li>(123) 456-7890</li>
+        <li>jportfolio@website.com</li>
+      </ul>
+    </div>
+  );
+}
---
+++ 00-practice-app/src/components/pages/Portfolio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <figure>
+          <h1>My Portfolio</h1>
+      </figure>
+
+      <figure>         
+          <h1>Horiseon</h1>
+      </figure>  
+
+      <figure> 
+          <h1>Code Quiz</h1>
+      </figure>
+    </div>
+  );
+}
